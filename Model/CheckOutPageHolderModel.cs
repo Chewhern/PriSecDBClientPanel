b413b9527cfb96a9d148c6cbfee0869adf9f7d19
Chrono <chrono_legionnaire@protonmail.com>
2021-08-14 13:02:13 +0800

    Add files via upload

---
+++ Model/CheckOutPageHolderModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriSecDBClientPanel.Model
+{
+    class CheckOutPageHolderModel
+    {
+        public String PayPalOrderID { get; set; }
+
+        public String CheckOutPageUrl { get; set; }
+
+        public String InvoiceID { get; set; }
+    }
+}
---
+++ Model/SpecialSelectDBModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriSecDBClientPanel.Model
+{
+    class SpecialSelectDBModel
+    {
+        public SealedDBCredentialModel MyDBCredentialModel { get; set; }
+
+        public String UniquePaymentID { get; set; }
+
+        public String Base64QueryString { get; set; }
+
+        public String[] Base64ParameterName { get; set; }
+
+        public String[] Base64ParameterValue { get; set; }
+    }
+}
