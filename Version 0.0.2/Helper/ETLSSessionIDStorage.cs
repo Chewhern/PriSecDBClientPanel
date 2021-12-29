2663b618508b96580f07124bbab55f2b98869908
Chrono <chrono_legionnaire@protonmail.com>
2021-12-29 15:54:46 +0800

    Add files via upload

---
+++ Version 0.0.2/Helper/ETLSSessionIDStorage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriSecDBClientPanel.Helper
+{
+    class ETLSSessionIDStorage
+    {
+        public static String ETLSID { get; set; }
+    }
+}
